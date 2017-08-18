d9411785d7bd5edb4423a0f98376c60f3cd01c68
meyacine <my.meddah@gmail.com>
2017-08-18 14:17:33 +0000

    added ID, and current time

--- dht11.py
+++ dht11.py
@@ -7,16 +7,22 @@
 import sys
 import Adafruit_DHT
 import requests
+import datetime
 
-url = 'http://server.technosofteam.com/pi/'
+
+DEVICE_ID = 'ABCDEF'
+url = 'http://server.technosofteam.com/pi/api/dht11/'
+
 
 try:
     while True:
+	 i = datetime.datetime.now()
+	 current_time_string = "%s-%s-%s %s:%s:%s" % (i.year, i.month, i.day, i.hour, i.minute, i.second)
          humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-         data = '{{"temp": "{0:0.1f}", "humidity": "{1:0.1f}"}} '.format(temperature, humidity)
+         data = '{{"id":"{0:s}", "temp": "{1:0.1f}", "humidity": "{2:0.1f}", "instant" : "{3:s}"}} '.format(DEVICE_ID, temperature, humidity, current_time_string)
          response = requests.post(url, data=data)
          print "sending : " + data
-         # print response.text
+         print response.text
 except KeyboardInterrupt:
     print "\Stopping capture"
 
